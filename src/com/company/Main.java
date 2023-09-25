012a13dd50632af4ed2eef1646dca42d7b73e480
Dzmitry <zhdv27@mail.ru>
2023-09-25 13:29:06 +0300

    main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,7 @@ public class Main  {
         System.out.println("hello");
         System.out.println("hello world");
         System.out.println("hello world!!");
+        System.out.println("hello world!!");
 
 
 
