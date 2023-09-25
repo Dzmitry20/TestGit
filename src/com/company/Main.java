be0dcd3d8a9df7627a0824c35aa40b64e499e070
Dzmitry <zhdv27@mail.ru>
2023-09-25 13:58:56 +0300

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,6 +16,8 @@ public class Main  {
         System.out.println();
         System.out.println();
         System.out.println();
+        System.out.println();
+        System.out.println();
 
         
 
